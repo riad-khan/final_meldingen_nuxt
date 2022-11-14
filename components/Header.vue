4ba6253d2306031e1bbb162f5eb79f4d735f09d7
riad-khan <rhkhan2367@gmail.com>
2022-11-15 00:53:01 +0600

    mobile menu js support

--- components/Header.vue
+++ components/Header.vue
@@ -5,7 +5,7 @@
       <div class="sidenav-header">
         <div class="logo">
           <h1>
-            <router-link to="/"><img alt="" src="@/assets/img/logo.svg"/></router-link>
+            <router-link to="/"><img alt="" src="@/assets/img/logo.svg" /></router-link>
           </h1>
         </div>
         <div class="close-sec">
@@ -14,19 +14,19 @@
       </div>
       <div class="menu-mobile">
         <ul>
-          <li :class="currentRouteName === '/' ? 'active':''">
+          <li :class="currentRouteName === '/' ? 'active' : ''">
             <nuxt-link to="/">Meldingen</nuxt-link>
           </li>
-          <li :class="currentRouteName === '/nieuws' ? 'active':''">
+          <li :class="currentRouteName === '/nieuws' ? 'active' : ''">
             <nuxt-link to="/nieuws">Nieuws</nuxt-link>
           </li>
-          <li :class="currentRouteName === '/contact' ? 'active':''">
+          <li :class="currentRouteName === '/contact' ? 'active' : ''">
             <router-link to="/contact">Contact</router-link>
           </li>
-          <li v-if="isAuth" :class="currentRouteName === '/dashboard' ? 'active':''">
+          <li v-if="isAuth" :class="currentRouteName === '/dashboard' ? 'active' : ''">
             <router-link to="/dashboard"><span>Profile</span></router-link>
           </li>
-          <li v-else :class="currentRouteName === '/login' ? 'active':''">
+          <li v-else :class="currentRouteName === '/login' ? 'active' : ''">
             <nuxt-link to="/login"><span>Account</span></nuxt-link>
           </li>
           <!--
@@ -53,27 +53,33 @@
           <div class="col-md-4 col-xs-8">
             <div class="logo">
               <h1>
-                <router-link to="/"><img alt="" src="@/assets/img/logo.svg"/></router-link>
+                <router-link to="/"><img alt="" src="@/assets/img/logo.svg" /></router-link>
               </h1>
             </div>
           </div>
           <div class="col-md-8 col-xs-4 textright">
+
+            <a href="#menu" @click.prevent="openNav" id="toggle"><span></span></a>
             <div class="menu">
-              <button class="openNav box-shadow" @click="openNav"><img src="@/assets/img/icon-hamburger.svg"></button>
+
+
+
+              <!-- <a  @click="openNav" id="toggle"><span></span></a> -->
+              <!-- <button class="openNav box-shadow" id="toggle" @click="openNav"><img src="@/assets/img/icon-hamburger.svg"></button>  -->
               <ul>
-                <li :class="currentRouteName === '/' ? 'active':''">
+                <li :class="currentRouteName === '/' ? 'active' : ''">
                   <nuxt-link to="/">Meldingen</nuxt-link>
                 </li>
-                <li :class="currentRouteName === '/nieuws' ? 'active':''">
+                <li :class="currentRouteName === '/nieuws' ? 'active' : ''">
                   <nuxt-link to="/nieuws">Nieuws</nuxt-link>
                 </li>
-                <li :class="currentRouteName === '/contact' ? 'active':''">
+                <li :class="currentRouteName === '/contact' ? 'active' : ''">
                   <router-link to="/contact">Contact</router-link>
                 </li>
-                <li v-if="isAuth" :class="currentRouteName === '/dashboard' ? 'active':''">
+                <li v-if="isAuth" :class="currentRouteName === '/dashboard' ? 'active' : ''">
                   <router-link to="/dashboard"><span>Profile</span></router-link>
                 </li>
-                <li v-else :class="currentRouteName === '/login' ? 'active':''">
+                <li v-else :class="currentRouteName === '/login' ? 'active' : ''">
                   <nuxt-link to="/login"><span>Account</span></nuxt-link>
                 </li>
                 <!--
@@ -102,19 +108,19 @@
 </template>
 
 <script>
- import {isAuth} from "../middlewares/auth";
+import { isAuth } from "../middlewares/auth";
 
 
 export default {
   name: "Header",
   data() {
     return {
-       isAuth: null,
+      isAuth: null,
     }
   },
 
   created() {
-    if(typeof window !== "undefined"){
+    if (typeof window !== "undefined") {
       this.isAuth = isAuth();
     }
   },
@@ -126,22 +132,107 @@ export default {
   },
   methods: {
     openNav() {
-      document.getElementById("mobileSidenav").style.right = "0";
-      var element = document.getElementById("body-class");
-      element.classList.add("sidenav-open");
+      let btn = document.getElementById('toggle');
+      this.toggleClass(btn, 'on');
+      return false;
     },
-    closeNav() {
-      document.getElementById("mobileSidenav").style.right = "-88%";
-      var element = document.getElementById("body-class");
-      setTimeout(function () {
-        element.classList.remove("sidenav-open");
-      }, 500);
+
+    hasClass(elem, className) {
+      return new RegExp(' ' + className + ' ').test(' ' + elem.className + ' ');
+    },
+    addClass(elem, className) {
+      if (!this.hasClass(elem, className)) {
+        elem.className += ' ' + className;
+      }
+    },
+    removeClass(elem, className) {
+      var newClass = ' ' + elem.className.replace(/[\t\r\n]/g, ' ') + ' ';
+      if (this.hasClass(elem, className)) {
+        while (newClass.indexOf(' ' + className + ' ') >= 0) {
+          newClass = newClass.replace(' ' + className + ' ', ' ');
+        }
+        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+      }
+    },
+
+    toggleClass(elem, className) {
+      var newClass = ' ' + elem.className.replace(/[\t\r\n]/g, " ") + ' ';
+      if (this.hasClass(elem, className)) {
+        while (newClass.indexOf(" " + className + " ") >= 0) {
+          newClass = newClass.replace(" " + className + " ", " ");
+        }
+        elem.className = newClass.replace(/^\s+|\s+$/g, '');
+      } else {
+        elem.className += ' ' + className;
+      }
     }
+
+
+    // openNav() {
+    //   document.getElementById("mobileSidenav").style.right = "0";
+    //   var element = document.getElementById("body-class");
+    //   element.classList.add("sidenav-open");
+    // },
+    // closeNav() {
+    //   document.getElementById("mobileSidenav").style.right = "-88%";
+    //   var element = document.getElementById("body-class");
+    //   setTimeout(function () {
+    //     element.classList.remove("sidenav-open");
+    //   }, 500);
+    // }
   }
 
 }
 </script>
-
 <style scoped>
+/* Important styles */
+#toggle {
+  display: block;
+  width: 28px;
+  height: 30px;
+  margin: 30px auto 10px;
+}
+
+#toggle span:after,
+#toggle span:before {
+  content: "";
+  position: absolute;
+  left: 0;
+  top: -9px;
+}
+#toggle span:after{
+  top: 9px;
+}
+#toggle span {
+  position: relative;
+  display: block;
+}
+
+#toggle span,
+#toggle span:after,
+#toggle span:before {
+  width: 100%;
+  height: 5px;
+  background-color: #888;
+  transition: all 0.3s;
+  backface-visibility: hidden;
+  border-radius: 2px;
+}
+
+/* on activation */
+#toggle.on span {
+  background-color: transparent;
+}
+#toggle.on span:before {
+  transform: rotate(45deg) translate(5px, 5px);
+}
+#toggle.on span:after {
+  transform: rotate(-45deg) translate(7px, -8px);
+}
+#toggle.on + #menu {
+  opacity: 1;
+  visibility: visible;
+}
+
 
 </style>
